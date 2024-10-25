975afb26e25ec144ddaa6dc5657a4eadd616b69d
Adriel Izoton de Faci <adriel.idf@gmail.com>
2024-10-24 22:25:19 -0300

    Basic page files, db template, query communication setup

---
+++ prisma/migrations/20241024215424_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "mobileNumber" TEXT NOT NULL,
+    "dateOfBirth" DATETIME NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
